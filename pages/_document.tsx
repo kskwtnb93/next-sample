2dde80980fb036014acb84ffe68a0e70bd46c7a8
keisuke watanabe <kskwtnb93@gmail.com>
2022-10-17 03:40:13 +0900

    styled-components

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {DocumentContext} from 'next/document'
+import {ServerStyleSheet} from 'styled-components'
+
+// デフォルトの Document 　を MyDocument で上書き
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const sheet = new ServerStyleSheet()
+		const originalRenderPage = ctx.renderPage
+
+		try {
+			ctx.renderPage = () => originalRenderPage({
+				enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+			})
+
+			// 初期値を流用
+			const initialProps = await Document.getInitialProps(ctx)
+
+			// initialProps に加えて、style を追加して返す。
+			return {
+				...initialProps,
+				styles: [
+					// もともとの style
+					initialProps.styles,
+					// styled-components の style
+					sheet.getStyleElement()
+				],
+			}
+		} finally {
+			sheet.seal()
+		}
+	}
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +14,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <H1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </H1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -68,5 +69,9 @@ const Home: NextPage = () => {
     </div>
   )
 }
+
+const H1 = styled.h1`
+	color: red;
+`
 
 export default Home
